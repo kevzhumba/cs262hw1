79502f601c2b345fcbe27ba81d137d0288c88004
howardg2000 <hjguo@college.harvard.edu>
2023-02-18 03:08:49 -0500

    Close smoothly on keyboard interrupt

--- wire_protocol/run_client.py
+++ wire_protocol/run_client.py
@@ -2,11 +2,13 @@ import client
 
 if __name__ == '__main__':
     client_instance = client.Client()
-    
+
     host = input('Enter host: ')
     port = int(input('Enter port: '))
-    
-    client_instance.connect(host, port)
-    client_instance.run()
-    
-    client_instance.close()
+
+    try:
+        client_instance.connect(host, port)
+        client_instance.run()
+    except KeyboardInterrupt:
+        client_instance.close()
+        print('Disconnected from server')
