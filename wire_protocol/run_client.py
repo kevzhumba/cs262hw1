c8838d2ea1f15863d5c23f4b992d9545216eddb4
howardg2000 <hjguo@college.harvard.edu>
2023-02-18 20:49:07 -0500

    Fix init

--- wire_protocol/run_client.py
+++ wire_protocol/run_client.py
@@ -1,13 +1,13 @@
 import client
 
 if __name__ == '__main__':
-    client_instance = client.Client()
-
     host = input('Enter host: ')
     port = int(input('Enter port: '))
 
+    client_instance = client.Client(host, port)
+
     try:
-        client_instance.connect(host, port)
+        client_instance.connect()
         client_instance.run()
     except KeyboardInterrupt:
         client_instance.close()
