c6bef6af15f00a3390cf6459c5b6c17408241b64
howardg2000 <hjguo@college.harvard.edu>
2023-02-19 20:08:01 -0500

    Specify protos and compile

---
+++ grpc/src/server_pb2.pyi
@@ -0,0 +1,82 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class CreateAccountRequest(_message.Message):
+    __slots__ = ["username"]
+    USERNAME_FIELD_NUMBER: _ClassVar[int]
+    username: str
+    def __init__(self, username: _Optional[str] = ...) -> None: ...
+
+class CreateAccountResponse(_message.Message):
+    __slots__ = ["status", "username"]
+    STATUS_FIELD_NUMBER: _ClassVar[int]
+    USERNAME_FIELD_NUMBER: _ClassVar[int]
+    status: str
+    username: str
+    def __init__(self, status: _Optional[str] = ..., username: _Optional[str] = ...) -> None: ...
+
+class DeleteAccountRequest(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
+
+class DeleteAccountResponse(_message.Message):
+    __slots__ = ["status"]
+    STATUS_FIELD_NUMBER: _ClassVar[int]
+    status: str
+    def __init__(self, status: _Optional[str] = ...) -> None: ...
+
+class ListAccountsRequest(_message.Message):
+    __slots__ = ["query"]
+    QUERY_FIELD_NUMBER: _ClassVar[int]
+    query: str
+    def __init__(self, query: _Optional[str] = ...) -> None: ...
+
+class ListAccountsResponse(_message.Message):
+    __slots__ = ["accounts", "status"]
+    ACCOUNTS_FIELD_NUMBER: _ClassVar[int]
+    STATUS_FIELD_NUMBER: _ClassVar[int]
+    accounts: _containers.RepeatedScalarFieldContainer[str]
+    status: str
+    def __init__(self, status: _Optional[str] = ..., accounts: _Optional[_Iterable[str]] = ...) -> None: ...
+
+class LogInRequest(_message.Message):
+    __slots__ = ["username"]
+    USERNAME_FIELD_NUMBER: _ClassVar[int]
+    username: str
+    def __init__(self, username: _Optional[str] = ...) -> None: ...
+
+class LogInResponse(_message.Message):
+    __slots__ = ["status", "username"]
+    STATUS_FIELD_NUMBER: _ClassVar[int]
+    USERNAME_FIELD_NUMBER: _ClassVar[int]
+    status: str
+    username: str
+    def __init__(self, status: _Optional[str] = ..., username: _Optional[str] = ...) -> None: ...
+
+class LogOffRequest(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
+
+class LogOffResponse(_message.Message):
+    __slots__ = ["status"]
+    STATUS_FIELD_NUMBER: _ClassVar[int]
+    status: str
+    def __init__(self, status: _Optional[str] = ...) -> None: ...
+
+class SendMessageRequest(_message.Message):
+    __slots__ = ["message", "recipient"]
+    MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    RECIPIENT_FIELD_NUMBER: _ClassVar[int]
+    message: str
+    recipient: str
+    def __init__(self, recipient: _Optional[str] = ..., message: _Optional[str] = ...) -> None: ...
+
+class SendMessageResponse(_message.Message):
+    __slots__ = ["status"]
+    STATUS_FIELD_NUMBER: _ClassVar[int]
+    status: str
+    def __init__(self, status: _Optional[str] = ...) -> None: ...
