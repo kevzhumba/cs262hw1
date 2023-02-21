4e0cab65e451147fa292594553051991482100e5
howardg2000 <hjguo@college.harvard.edu>
2023-02-21 02:22:03 -0500

    Add login/delete fail tests

--- wire_protocol/server_test.py
+++ wire_protocol/server_test.py
@@ -9,6 +9,7 @@ TEST_HOST = "127.0.0.1"
 TEST_PORT = 6000
 TEST_PROTOCOL = protocol_instance
 
+
 class ServerTest(unittest.TestCase):
     def setUp(self):
         self.server = Server(TEST_HOST, TEST_PORT, TEST_PROTOCOL)
@@ -25,51 +26,59 @@ class ServerTest(unittest.TestCase):
     def test_create_account_success(self):
         args = {"username": "joseph"}
         joseph_socket = MagicMock()
-        response = self.server.process_create_account(args, joseph_socket, threading.Lock())
+        response = self.server.process_create_account(
+            args, joseph_socket, threading.Lock())
         self.assertEqual(response['status'], 'Success')
         self.assertTrue(("joseph" in self.server.account_list))
         self.assertTrue(("joseph" in self.server.logged_in.keys()))
-    
+
     def test_create_account_fail_exists(self):
         args = {"username": "kevin"}
         joseph_socket = MagicMock()
-        response = self.server.process_create_account(args, joseph_socket, threading.Lock())
+        response = self.server.process_create_account(
+            args, joseph_socket, threading.Lock())
         self.assertEqual(response['status'], 'Error: Account already exists.')
-    
+
     def test_create_account_fail_logged_in(self):
         args = {"username": "joseph"}
         (joseph_socket, lock) = self.server.logged_in["kevin"]
-        response = self.server.process_create_account(args, joseph_socket, lock)
-        self.assertEqual(response['status'], 'Error: User can\'t create an account while logged in.')
-    
+        response = self.server.process_create_account(
+            args, joseph_socket, lock)
+        self.assertEqual(
+            response['status'], 'Error: User can\'t create an account while logged in.')
+
     def test_login_success(self):
         args = {"username": "kevin"}
         self.server.logged_in.pop("kevin")
         mock_kevin_lock = MagicMock()
         mock_kevin_socket = MagicMock()
-        response = self.server.process_login(args, mock_kevin_socket, mock_kevin_lock)
+        response = self.server.process_login(
+            args, mock_kevin_socket, mock_kevin_lock)
         self.assertEqual(response['status'], 'Success')
         self.assertTrue("kevin" in self.server.logged_in.keys())
 
     def test_login_fail_doesnt_exist(self):
         args = {"username": "joseph"}
         joseph_socket = MagicMock()
-        response = self.server.process_login(args, joseph_socket, threading.Lock())
+        response = self.server.process_login(
+            args, joseph_socket, threading.Lock())
         self.assertEqual(response['status'], 'Error: Account does not exist.')
-    
+
     def test_login_fail_someone_logged_in(self):
         args = {"username": "kevin"}
         mock_kevin_lock = threading.Lock()
         mock_kevin_socket = MagicMock()
-        response = self.server.process_login(args, mock_kevin_socket, mock_kevin_lock)
-        self.assertEqual(response['status'], 'Error: Someone else is logged into that account.')
-    
+        response = self.server.process_login(
+            args, mock_kevin_socket, mock_kevin_lock)
+        self.assertEqual(
+            response['status'], 'Error: Someone else is logged into that account.')
+
     def test_list_account_success(self):
         args = {'query': "kevin"}
         response = self.server.process_list_accounts(args)
         self.assertEqual(response['status'], 'Success')
         self.assertEqual(response['accounts'], 'kevin')
-    
+
     def test_list_account_regex_bad(self):
         args = {'query': "["}
         response = self.server.process_list_accounts(args)
@@ -77,28 +86,43 @@ class ServerTest(unittest.TestCase):
 
     def test_send_msg_success(self):
         args = {'recipient': 'kevin', 'message': 'hello'}
-        response = self.server.process_send_msg(args, self.server.logged_in['kevin'][0], self.server.logged_in['kevin'][1])
+        response = self.server.process_send_msg(
+            args, self.server.logged_in['kevin'][0], self.server.logged_in['kevin'][1])
         self.assertEqual(response['status'], 'Success')
         self.assertTrue("kevin" in self.server.undelivered_msg.keys())
-    
+
     def test_send_msg_failure_no_recipient(self):
         args = {'recipient': 'joseph', 'message': 'hello'}
-        response = self.server.process_send_msg(args, self.server.logged_in['kevin'][0], self.server.logged_in['kevin'][1])
-        self.assertEqual(response['status'], 'Error: The recipient of the message does not exist.')
+        response = self.server.process_send_msg(
+            args, self.server.logged_in['kevin'][0], self.server.logged_in['kevin'][1])
+        self.assertEqual(
+            response['status'], 'Error: The recipient of the message does not exist.')
 
     def test_delete_account_success(self):
-        response = self.server.process_delete_account(self.server.logged_in['kevin'][0], self.server.logged_in['kevin'][1])
+        response = self.server.process_delete_account(
+            self.server.logged_in['kevin'][0], self.server.logged_in['kevin'][1])
         self.assertEqual(response['status'], 'Success')
         self.assertFalse('kevin' in self.server.account_list)
-    
+
+    def test_delete_account_fail(self):
+        joseph_socket = MagicMock()
+        response = self.server.process_delete_account(
+            joseph_socket, threading.Lock())
+        self.assertEqual(
+            response['status'], 'Error: Need to be logged in to delete your account.')
+
     def test_logoff_success(self):
-        response = self.server.process_logoff(self.server.logged_in['kevin'][0], self.server.logged_in['kevin'][1])
+        response = self.server.process_logoff(
+            self.server.logged_in['kevin'][0], self.server.logged_in['kevin'][1])
         self.assertEqual(response['status'], 'Success')
         self.assertFalse('kevin' in self.server.logged_in.keys())
 
-
-
+    def test_logoff_fail(self):
+        joseph_socket = MagicMock()
+        response = self.server.process_logoff(joseph_socket, threading.Lock())
+        self.assertEqual(
+            response['status'], 'Error: Need to be logged in to log out of your account.')
 
 
 if __name__ == '__main__':
-    unittest.main()        
+    unittest.main()
